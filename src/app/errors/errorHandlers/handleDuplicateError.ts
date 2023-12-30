6c13a8a7ef8ad92aaf71d4bffd159b2d6467ed07
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2023-12-30 10:04:08 +0600

    final adjustments

--- src/app/errors/errorHandlers/handleDuplicateError.ts
+++ src/app/errors/errorHandlers/handleDuplicateError.ts
@@ -5,7 +5,9 @@ import { TErrorResponse } from '../../interface/errors';
 
 export const handleDuplicateError = (err: any): TErrorResponse => {
   const message = 'Duplicate Key error!';
-  const errorMessage = `'${err.keyValue.title}' already exists!`;
+  const errorMessage = `'${
+    err.keyValue.username || err.keyValue.email
+  }' already exists!`;
   const statusCode = httpStatus.BAD_REQUEST;
   const errorDetails = err;
 
